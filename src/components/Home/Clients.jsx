c2b67b5a9547ab2609aa2d50283259c420d9fd55
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-06-22 17:40:43 +0530

    mobile responsive

--- src/components/Home/Clients.jsx
+++ src/components/Home/Clients.jsx
@@ -4,8 +4,8 @@ export default function Clients() {
   return (
     <div className=' sm:h-64 md:h-11/12 sm:mt-10 md:mt-16 bg-zinc-100'>
        <h1 className='text-center pt-4 text-3xl font-semibold'>Our Clients</h1>
-       {/* <marquee behavior="alternate" direction="" pauseOnHover='true' speed='50' loop='2' > */}
-        <div className='flex justify-between mt-8 mb-10 sm:bg-red-800 items-start'>
+       <marquee behavior="alternate" direction="" pauseOnHover='true' speed='50' loop='2' >
+        <div className='flex justify-between mt-8 mb-10  items-start'>
         <div className='md:h-32 md:w-48 hover:scale-105 h-32 w-[600px] ml-8 '>            
                <img className='md:h-32 md:w-48 rounded-lg h-24 w-[600px]' src="https://th.bing.com/th/id/OIP.hCuZce4tatBhkTjk14qI4QAAAA?rs=1&pid=ImgDetMain" alt="" />  
            </div>
@@ -34,7 +34,7 @@ export default function Clients() {
        
         
         </div>
-       {/* </marquee> */}
+       </marquee>
     </div>
   )
 }
