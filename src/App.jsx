11eb7411d60a9d2b8fcd64d881b04f7b06e52c03
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-06-24 18:08:51 +0530

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
  
 import React from 'react'
 import Homepage from './components/Home/Homepage'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 import About from './components/Home/About'
 import Services from './components/Home/Services'
+import Products from './components/pages/Products'
+import Contact from './components/pages/Contact'
 
 
 function App() {
  
 
   return (
-    <Homepage/>
+    <BrowserRouter>
+            
+    <NavBar/>
+
+<Routes>
+ 
+<Route path='/'  element={  <div>
+    <Caraousal/>
+    <About/>
+    <Clients/>
+    <Services/>
+ </div>} />
+<Route path='/about'  element={ <About/>} />
+<Route path='/services'  element={ <Services/>} />
+<Route path='/products'  element={ <Products/>} />
+<Route path='/contact'  element={ <Contact/>} />
+
+</Routes>
+    <FooterComponent/>
+</BrowserRouter>
   )
 }
 
