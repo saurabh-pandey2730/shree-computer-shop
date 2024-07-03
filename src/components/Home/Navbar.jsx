47c3fed20cc736319721bef2091aff7a59554771
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-07-03 21:14:21 +0530

    navigation resolved

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { Button, Navbar } from "flowbite-react"
 // import logo from '../../assets/shree-logo.png'
 import logo from '../../assets/logo-scs-.png' 
+import { Link } from 'react-router-dom'
 export default function NavBar() {
   return (
     <Navbar fluid rounded className='bg-[#f5f5f5] sticky top-0 z-10 ; '>
        <Navbar.Brand href="http">
          <img src={logo} className=" lg:h-16 sm:h-20 h-16" alt="Shree-computer-shop " />
-         <span className='font-bold ml-9 sm:ml-0 md:ml-0 px-2 py-1 lg:ml-16 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'> Shree Computer Shop</span> 
+         <Link to='/' className='font-bold ml-9 sm:ml-0 md:ml-0 px-2 py-1 lg:ml-16 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'> Shree Computer Shop</Link> 
        </Navbar.Brand>
       <div className="flex md:order-2">
-         <Button gradientDuoTone='purpleToBlue' className='outline mr-14 hidden md:inline'>Rental Services</Button>
+         <Link to='/contact' gradientDuoTone='purpleToBlue' className='outline mr-14 hidden md:inline'>Rental Services</Link>
       <Navbar.Toggle />
     </div>
     <Navbar.Collapse>
